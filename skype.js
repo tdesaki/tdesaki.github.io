98d32603fd043b498831152da1a6849241698a39
Takeya <takeyad@gmail.com>
2016-08-16 14:22:38 +0000

    portfolio

---
+++ skype.js
@@ -0,0 +1,10 @@
+<script type="text/javascript" src="https://secure.skypeassets.com/i/scom/js/skype-uri.js"></script>
+<div id="SkypeButton_Call_takeyafly_1">
+ <script type="text/javascript">
+ Skype.ui({
+ "name": "chat",
+ "element": "SkypeButton_Call_takeyafly_1",
+ "participants": ["takeyafly"]
+ });
+ </script>
+</div>
